e2afb51a2027c8f6609931834e0e5683f9237842
FlamingGenius <deathcrow4561@gmail.com>
2017-09-09 10:26:56 +0000

    Revert "Improved website"
    This reverts commit 815284260a128d7debba5a1830ca13b121d8e64e.

--- web-server/index.php
+++ web-server/index.php
@@ -1,179 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
+	<title>Clash Of Flames</title>
+	<meta charset="utf-8">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+	<script	src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 
-    <title>Clash of Flames</title>
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-    <!-- Bootstrap Core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Theme CSS -->
-    <link href="css/clean-blog.css" rel="stylesheet">
-	<link href="css/custom.css" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
-<style>
-	.inactive{
-		background:rgba(0,0,0,0.6);
-		opacity: 0;
-		z-index: 99;
-		content: 'Coming Soon';
-		color: #fff;
-	}
-</style>
 <body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    Menu <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand" href="index.html">Clash of Flames | Dedicated Server | Status: <span style="color:#49fb35">Online</span></a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="http://cof.ftp.sh">Home</a>
-                    </li>
-                    <li>
-                        <a href="http://cof.ftp.sh/forum/">Forum</a>
-                    </li>
-                    <li>
-                        <a href="http://cof.ftp.sh/hof/">Hall of Fame</a>
-                    </li>
-            
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
-    <!-- Page Header -->
-    <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('img/Chunk 1.png')">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <div class="site-heading">
-                        <h1>CoF</h1>
-                        <hr class="small">
-                        <span class="subheading">Now available on</span>
-						<span class="subheading">Android</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-
-    <!-- Main Content -->
-
-			    <section id="plans">
-        <div class="container">
-            <div class="row">
-
-                <!-- item -->
-                <div class="col-md-4 text-center inactive">
-                </div>
-                <!-- /item -->
-
-                <!-- item -->
-                <div class="col-md-4 text-center">
-                    <div class="panel panel-success panel-pricing">
-                        <div class="panel-heading">
-                            <i class="fa fa-server"></i>
-                            <h3></h3>
-                        </div>
-                        <div class="panel-body text-center">
-                            <p><strong>US1</strong></p>
-                        </div>
-                        <ul class="list-group text-center">
-                            <li class="list-group-item"><i class="fa fa-check"></i> Custom Mods</li>
-                            <li class="list-group-item"><i class="fa fa-check"></i> 2vCPU Core</li>
-                            <li class="list-group-item"><i class="fa fa-check"></i> 7.5GB ram</li>
-                        </ul>
-                        <div class="panel-footer">
-                            <a class="btn btn-lg btn-block btn-success" href="#" disabled>Download iOS</a>
-							<a class="btn btn-lg btn-block btn-success" href="http://dataurbia.com/Tjy">Download Android</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- /item -->
-
-                <!-- item -->
-                <div class="col-md-4 text-center inactive">
-                </div>
-                <!-- /item -->
-
-            </div>
-        </div>
-    </section>
-
-    <hr>
-
-    <!-- Footer -->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <ul class="list-inline text-center">
-                        <li class="hidden">
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li class="hidden">
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li class="hidden">
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                    </ul>
-                    <p class="copyright text-muted">Copyright &copy; FlameForged 2017</p>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-    <!-- jQuery -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Contact Form JavaScript -->
-    <script src="js/jqBootstrapValidation.js"></script>
-
-    <!-- Theme JavaScript -->
-    <script src="js/clean-blog.min.js"></script>
-
+	<header>
+		<a href="https://ucslist.com/Clash_of_Flames" target="_blank">
+			<img src="https://ucslist.com/draw/image.php?param=Q2xhc2hfb2ZfRmxhbWVz&show=Kk4qTSpTKlIqVQ==&w=728&h=90" width="100%">
+		</a>
+	</header>
+	<nav>
+		<?php
+			require_once("includes/nav.php"); 
+		?>
+	</nav>
+	<main>
+	<h1>Clash of Flames</h1>
+		<div style="float:right">
+			<script type="text/javascript" src="http://feedjit.com/serve/?vv=1515&amp;tft=3&amp;dd=0&amp;wid=&amp;pid=0&amp;proid=0&amp;bc=FFFFFF&amp;tc=000000&amp;brd1=012B6B&amp;lnk=135D9E&amp;hc=FFFFFF&amp;hfc=2853A8&amp;btn=C99700&amp;ww=160&amp;wne=10&amp;srefs=0"></script><noscript><a href="http://feedjit.com/">Live Traffic Stats</a></noscript>
+		</div>
+		<p>
+			Clash of Flames(CoF) is a dedicated server for clash of clans run by FlameForged developed by FlamingGenius. Server is online 24/7 and recieves weekly maintenance breaks to ensure quality and stability
+		</p>
+		
+		<h3>Server Specs</h3>
+		<ul>
+			<li>750k Starting Gems</li>
+			<li>150m Starting Gold</li>
+			<li>150m Starting Elixier</li>
+			<li>24/7 Dedicated Server</li>
+			<li>50gb storage</li>
+			<li>10gb bandwidth</li>
+			<li>2vCPU core</li>
+			<li>Solid State Drive</li>
+		</ul>	
+	</main>
+	<footer>
+		<p><b>Copyright &copy; 2017 FlameForged</b></p>
+	</footer>
 </body>
-
 </html>
